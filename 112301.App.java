b472f9efb4c2e7c3b4d42759583ba6d5b370a101
hujinhai666 <47549127+hujinhai666@users.noreply.github.com>
2020-11-23 11:22:41 +0800

    Update 112301.App.java
    当用户输入1时，生成随机坐标。

--- 112301.App.java
+++ 112301.App.java
@@ -9,10 +9,9 @@ public class App {
         while(true) {
             Scanner scanner = new Scanner(System.in);
             int sr = scanner.nextInt();
-            int x = (int) (Math.random() * 100 % 100);
-            int y = (int) (Math.random() * 100 % 100);
             if (sr == 1) {
-                System.out.println("X坐标为：" + x + "\n" + "Y坐标为：" + y);
+                System.out.println("X坐标为：" + (int) (Math.random() * 100 % 100));
+                System.out.println("Y坐标为：" + (int) (Math.random() * 100 % 100));
             }else{
                 System.out.println("无效数值,请输入1");
             }
